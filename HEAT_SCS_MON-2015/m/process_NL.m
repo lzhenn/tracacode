12c2e20823427830ec9fdf1ef8ee0d00d68037d9
Novarizark <dennis-1991@qq.com>
2015-10-23 21:50:05 +0800

    matlab refresh

--- HEAT_SCS_MON-2015/m/process_NL.m
+++ HEAT_SCS_MON-2015/m/process_NL.m
@@ -4,10 +4,13 @@ function process_NL()
 	close all;
 	echo off;	
     %filename = fullfile('..','data','obv','F182013.v4c_web.stable_lights.avg_vis.tif');		%date dir name
-    filename = 'D:/F182013.v4c_web.stable_lights.avg_vis.tif';		%date dir name
+    filename = 'D:/cache/F182013.v4c_web.stable_lights.avg_vis.tif';		%date dir name
 	im0=imread(filename);
     %im0=imadjust(im0,[0.2 0.5], [0.0 0.4]);
     %im0(find(im0<0.1))=1.0;
     %imhist(im0(find(im0>0.1)));
-    size(im0)
-    imhist(im0)
+    %imtool(im0)
+    %imtool(im0(3601:7800,33601:38400))
+    fid=fopen('D:/cache/F182013_EA.dat','wb');
+    COUNT=fwrite(fid,im0(3601:7800,33601:38400),'int')
+    fclose(fid);
