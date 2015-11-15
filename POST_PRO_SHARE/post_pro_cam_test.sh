1620a8ec19f1ee06692f13331a421d794357674e
Novarizark <dennis-1991@qq.com>
2015-11-15 19:43:23 +0800

    newer postprocess

--- POST_PRO_SHARE/post_pro_cam_test.sh
+++ POST_PRO_SHARE/post_pro_cam_test.sh
@@ -14,19 +14,20 @@
 PRE_DIR=\"/home/yangsong3/data/model/CESM_CTRL/B2000_F19G16_CAM4_CTRL/pre/\"
 
 # Path of the post processed data
-PRO_DIR=\"/home/yangsong3/data/model/CESM_CTRL/B2000_F19G16_CAM4_CTRL/pro/\"
+PRO_DIR=\"/home/yangsong3/L_Zealot/temp/\"
 
 # Case name
 CASENAME=\"B2000_f19g16_CP_CTRL\"
 
 
 # Names of 2D fields
-FDNAME2D="(/\"PRECL\",\"PRECC\",\"LHFLX\",\"PS\",\"PSL\",\"QFLX\",\"TS\",\"TMQ\"/)" #often use
+#FDNAME2D="(/\"PRECL\",\"PRECC\",\"LHFLX\",\"PS\",\"PSL\",\"QFLX\",\"TS\",\"TMQ\"/)" #often use
 
 # Names of 3D fields
-FDNAME3D="(/\"U\",\"V\",\"T\",\"OMEGA\",\"Q\",\"RELHUM\",\"Z3\"/)" #often use
+#FDNAME3D="(/\"U\",\"V\",\"T\",\"OMEGA\",\"Q\",\"RELHUM\",\"Z3\"/)" #often use
+FDNAME3D="(/\"Z3\"/)" #often use
 
-FDNAME3D_HY="(/\"U\",\"V\",\"T\",\"OMEGA\",\"Q\",\"RELHUM\",\"Z3\",\"DTCOND\"/)" # hybrid coordinate
+#FDNAME3D_HY="(/\"U\",\"V\",\"T\",\"OMEGA\",\"Q\",\"RELHUM\",\"Z3\",\"DTCOND\"/)" # hybrid coordinate
 
 # Layers of 3D fields
 # CAM4 = 26; CAM5 = 30
@@ -41,22 +42,22 @@ PLEV="(/925,850,700,500,200/)"
 FRSTYEAR=250
 
 # Last year of the subset
-LSTYEAR=349
+LSTYEAR=255
 
 # Process fig flag
 #FIGFLAG=FALSE
 
 #-----------------------------------------------------------
 
-#Output post processed 2D fields
-ncl pre_dir=$PRE_DIR            \
-    pro_dir=$PRO_DIR            \
-    fdname2d=$FDNAME2D          \
-    frstyear=$FRSTYEAR          \
-    lstyear=$LSTYEAR            \
-    case_name=$CASENAME         \
-    ./ncl/take_2D_from_raw_data-150921.ncl
-
+##Output post processed 2D fields
+#ncl pre_dir=$PRE_DIR            \
+#    pro_dir=$PRO_DIR            \
+#    fdname2d=$FDNAME2D          \
+#    frstyear=$FRSTYEAR          \
+#    lstyear=$LSTYEAR            \
+#    case_name=$CASENAME         \
+#    ./ncl/take_2D_from_raw_data-150921.ncl
+#
 #Output post processed 3D fields
 ncl pre_dir=$PRE_DIR            \
     pro_dir=$PRO_DIR            \
@@ -66,16 +67,16 @@ ncl pre_dir=$PRE_DIR            \
     frstyear=$FRSTYEAR          \
     lstyear=$LSTYEAR            \
     case_name=$CASENAME         \
-    ./ncl/take_3D_from_raw_data-150921.ncl
+    ./ncl/take_3D_from_raw_data-grads-151114.ncl
 
 
-#Output post processed 3D fields
-ncl pre_dir=$PRE_DIR            \
-    pro_dir=$PRO_DIR            \
-    fdname3d=$FDNAME3D_HY       \
-    frstyear=$FRSTYEAR          \
-    lstyear=$LSTYEAR            \
-    case_name=$CASENAME         \
-    ./ncl/take_3D_hybrid_from_raw_data-150921.ncl
-
+##Output post processed 3D fields
+#ncl pre_dir=$PRE_DIR            \
+#    pro_dir=$PRO_DIR            \
+#    fdname3d=$FDNAME3D_HY       \
+#    frstyear=$FRSTYEAR          \
+#    lstyear=$LSTYEAR            \
+#    case_name=$CASENAME         \
+#    ./ncl/take_3D_hybrid_from_raw_data-150921.ncl
+#
 
