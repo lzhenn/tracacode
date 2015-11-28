f13d7f117de5ff93512801a453bfe8d6b5845824
Novarizark <dennis-1991@qq.com>
2015-11-28 09:41:22 +0800

    151128

--- POST_PRO_SHARE/post_pro_cam_grads.sh
+++ POST_PRO_SHARE/post_pro_cam_grads.sh
@@ -11,39 +11,39 @@
 # Path of the original data
 # Caution: DO NOT DELETE \" IN STRING!
 #PRE_DIR=\"/HOME/sysu_hjkx_ys/WORKSPACE/L_Zealot/cesm/B/B2000_f09_CAM5_spin-up/run/\"
-PRE_DIR=\"/users/yangsong3/cdh/cam4/INDEX2-16-V4/exe/\"
+PRE_DIR=\"/HOME/sysu_hjkx_ys/WORKSPACE/data/model/L_Zealot/SCS_ANNCYC-2015/pre/B2000_f09_CAM5PM_spin-up/\"
 
 # Path of the post processed data
-PRO_DIR=\"/users/yangsong3/cdh/cam4/postdata/\"
+PRO_DIR=\"/HOME/sysu_hjkx_ys/WORKSPACE/data/model/L_Zealot/SCS_ANNCYC-2015/pro/\"
 
 # Case name
-CASENAME=\"INDEX2-16-V4\"
+CASENAME=\"B2000_f09_CAM5PM_spin-up\"
 
 
 # Names of 2D fields
-#FDNAME2D="(/\"PRECL\",\"PRECC\",\"LHFLX\",\"PS\",\"PSL\",\"QFLX\",\"TS\",\"TMQ\"/)" #often use
-FDNAME2D="(/\"TS\"/)" #often use
+FDNAME2D="(/\"PRECL\",\"PRECC\",\"LHFLX\",\"PS\",\"PSL\",\"QFLX\",\"TS\",\"TMQ\"/)" #often use
+#FDNAME2D="(/\"TS\"/)" #often use
 
 # Names of 3D fields
-#FDNAME3D="(/\"U\",\"V\",\"T\",\"OMEGA\",\"Q\",\"RELHUM\",\"Z3\"/)" #often use
-FDNAME3D="(/\"U\",\"T\",\"Z3\"/)" #often use
+FDNAME3D="(/\"U\",\"V\",\"T\",\"OMEGA\",\"Q\",\"RELHUM\",\"Z3\"/)" #often use
+#FDNAME3D="(/\"U\",\"T\",\"Z3\"/)" #often use
 
 #FDNAME3D_HY="(/\"U\",\"V\",\"T\",\"OMEGA\",\"Q\",\"RELHUM\",\"Z3\",\"DTCOND\"/)" # hybrid coordinate
 
 # Layers of 3D fields
 # CAM4 = 26; CAM5 = 30
-LAYERS=26
+LAYERS=30
 
 # Output specific pressure layers
 # CAUTION: Do not leave species between element!
-#PLEV="(/925,850,700,600,500,400,300,200,100,50/)"
-PLEV="(/500,300,200/)"
+PLEV="(/925,850,700,600,500,400,300,200,100,50/)"
+#PLEV="(/500,300,200/)"
 
 # First year of the subset
-FRSTYEAR=1
+FRSTYEAR=204
 
 # Last year of the subset
-LSTYEAR=70
+LSTYEAR=208
 
 # Process fig flag
 #FIGFLAG=FALSE
---
+++ POST_PRO_SHARE/ppost_pro_cam_fig.sh
@@ -0,0 +1,102 @@
+#!/bin/sh
+#-----------------------------------------------
+#   This is a shell script for configuring the
+# basic p-post processing tool of CAM model, 
+# You should set the basic parameters as below. 
+# 
+# Good Luck!
+#
+# Basic Figures
+#       ----    UV850, with sig    
+#       ----    UV850, without sig    
+#       ----    Pr, with sig
+#       ----    Pr, without sig
+#       ----    UV850 + Pr, with sig
+#       ----    UV850 + Pr, without sig
+#
+#
+#
+#
+#
+#               Last Modified on  2015-11-27
+#               A L_Zealot Product
+#-----------------------------------------------
+
+
+# Path of the original data
+# Caution: DO NOT DELETE \" IN STRING!
+#PRE_DIR=\"/HOME/sysu_hjkx_ys/WORKSPACE/L_Zealot/cesm/B/B2000_f09_CAM5_spin-up/run/\"
+PRO_CTRL_DIR=\"/HOME/sysu_hjkx_ys/WORKSPACE/data/model/L_Zealot/SCS_ANNCYC-2015/pro/\"
+
+# CTRL Case name
+CTRL_CASENAME=\"B2000_f09_CAM5PM_spin-up\"
+
+
+# Path of the post processed data
+PRO_SEN_DIR=\"/HOME/sysu_hjkx_ys/WORKSPACE/data/model/L_Zealot/SCS_ANNCYC-2015/pro/\"
+
+# SEN case name
+SEN_CASENAME=\"B2000_f09_CAM5PM_SCS_ANNCYC\"
+
+# Path of outfig
+FIG_PATH=\"/HOME/sysu_hjkx_ys/WORKSPACE/L_Zealot/project/BRUTAL-TRANS-2015/fig/auto_fig\"
+
+# History file first year
+FFRSTYEAR=204
+
+# History file last year
+FLSTYEAR=208
+
+# Subset first year
+SUB_FRSTYEAR=204
+
+# Subset last year
+SUB_LSTYEAR=208
+
+# Range of the map, R_FLAG: regional flag, 1 for regional
+R_FLAG=1
+
+#-----------------------------------------------------------
+
+if  [ $R_FLAG == 1 ] ; then
+#EA
+    LATS=-5.
+    LATN=45.
+    LONW=90.
+    LONE=160.
+else
+    LATS=-90.
+    LATN=90.
+    LONW=0.
+    LONE=360.
+fi
+
+
+#Output post processed 2D fields
+ncl pro_ctrl_dir=$PRO_CTRL_DIR              \
+    ctrl_casename=$CTRL_CASENAME            \
+    pro_sen_dir=$PRO_SEN_DIR                \
+    sen_casename=$SEN_CASENAME              \
+    ffrstyear=$FFRSTYEAR                    \
+    flstyear=$FLSTYEAR                      \
+    sub_frstyear=$SUB_FRSTYEAR              \
+    sub_lstyear=$SUB_LSTYEAR                \
+    lats=$LATS                              \
+    latn=$LATN                              \
+    lonw=$LONW                              \
+    lone=$LONE                              \
+    fig_path=$FIG_PATH                      \
+    ./ncl/draw_diff-season-850UV_Pr_SEN-CTRL-20151127.ncl
+
+    
+
+##Output post processed 3D fields
+#ncl pre_dir=$PRE_DIR            \
+#    pro_dir=$PRO_DIR            \
+#    fdname3d=$FDNAME3D_HY       \
+#    frstyear=$FRSTYEAR          \
+#    lstyear=$LSTYEAR            \
+#    case_name=$CASENAME         \
+#    ./ncl/take_3D_hybrid_from_raw_data-150921.ncl
+
+
