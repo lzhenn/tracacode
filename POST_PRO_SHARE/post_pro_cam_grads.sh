47ae4c09d64d8983deb9ffd78859baf1b34c35e1
Novarizark <dennis-1991@qq.com>
2016-01-03 03:01:24 +0800

    160103

--- POST_PRO_SHARE/post_pro_cam_grads.sh
+++ POST_PRO_SHARE/post_pro_cam_grads.sh
@@ -10,19 +10,18 @@
 
 # Path of the original data
 # Caution: DO NOT DELETE \" IN STRING!
-#PRE_DIR=\"/HOME/sysu_hjkx_ys/WORKSPACE/L_Zealot/cesm/B/B2000_f09_CAM5_spin-up/run/\"
-PRE_DIR=\"/users/yangsong3/cdh/cam4/INDEX2/exe/\"
+PRE_DIR=\"/HOME/sysu_hjkx_ys/WORKSPACE/L_Zealot/cesm/B/B2000_f09_CAM5PM_spin-up/run/\"
+#PRE_DIR=\"/HOME/sysu_hjkx_ys/WORKSPACE/data/model/L_Zealot/SCS_ANNCYC-2015/pre/B2000_f09_CAM5PM_SCS_ANNCYC/\"
 
 # Path of the post processed data
-PRO_DIR=\"/users/yangsong3/cdh/cam4/postdata/\"
+PRO_DIR=\"/HOME/sysu_hjkx_ys/WORKSPACE/data/model/B2000_f09_CAM5PM_spin-up/\"
 
 # Case name
-CASENAME=\"INDEX2\"
+CASENAME=\"B2000_f09_CAM5PM_spin-up\"
 
 
 # Names of 2D fields
-# FDNAME2D="(/\"PRECL\",\"PRECC\",\"LHFLX\",\"PS\",\"PSL\",\"QFLX\",\"TS\",\"TMQ\"/)" #often use
-FDNAME2D="(/\"PRECL\",\"PRECC\"/)"
+FDNAME2D="(/\"PRECL\",\"PRECC\",\"LHFLX\",\"PS\",\"PSL\",\"QFLX\",\"TS\",\"TMQ\"/)" #often use
 #FDNAME2D="(/\"TS\"/)" #often use
 
 # Names of 3D fields
@@ -33,7 +32,7 @@ FDNAME3D="(/\"U\",\"V\",\"T\",\"OMEGA\",\"Q\",\"RELHUM\",\"Z3\"/)" #often use
 
 # Layers of 3D fields
 # CAM4 = 26; CAM5 = 30
-LAYERS=26
+LAYERS=30
 
 # Output specific pressure layers
 # CAUTION: Do not leave species between element!
@@ -41,30 +40,26 @@ PLEV="(/925,850,700,600,500,400,300,200,100,50/)"
 #PLEV="(/500,300,200/)"
 
 # First year of the subset
-FRSTYEAR=1
+FRSTYEAR=201
 
 # Last year of the subset
-LSTYEAR=70
+LSTYEAR=250
 
-# Process flag
-FLAG_2D=1
-FLAG_3D=0
-FLAG_3DHY=0
+# Process fig flag
+#FIGFLAG=FALSE
 
 #-----------------------------------------------------------
 
 #Output post processed 2D fields
-if  [ $FLAG_2D == 1 ] ; then
-    ncl pre_dir=$PRE_DIR            \
-        pro_dir=$PRO_DIR            \
-        fdname2d=$FDNAME2D          \
-        frstyear=$FRSTYEAR          \
-        lstyear=$LSTYEAR            \
-        case_name=$CASENAME         \
-        ./ncl/take_2D_from_raw_data-150921.ncl
-fi
+ncl pre_dir=$PRE_DIR            \
+    pro_dir=$PRO_DIR            \
+    fdname2d=$FDNAME2D          \
+    frstyear=$FRSTYEAR          \
+    lstyear=$LSTYEAR            \
+    case_name=$CASENAME         \
+    ./ncl/take_2D_from_raw_data-150921.ncl
+
 #Output post processed 3D fields
-if  [ $FLAG_3D == 1 ] ; then
 ncl pre_dir=$PRE_DIR            \
     pro_dir=$PRO_DIR            \
     fdname3d=$FDNAME3D          \
@@ -74,16 +69,15 @@ ncl pre_dir=$PRE_DIR            \
     lstyear=$LSTYEAR            \
     case_name=$CASENAME         \
     ./ncl/take_3D_from_raw_data-grads-151114.ncl
-fi
 
-#Output post processed 3D fields
-if  [ $FLAG_3DHY == 1 ] ; then
-    ncl pre_dir=$PRE_DIR            \
-        pro_dir=$PRO_DIR            \
-        fdname3d=$FDNAME3D_HY       \
-        frstyear=$FRSTYEAR          \
-        lstyear=$LSTYEAR            \
-        case_name=$CASENAME         \
-        ./ncl/take_3D_hybrid_from_raw_data-150921.ncl
-fi
+
+##Output post processed 3D fields
+#ncl pre_dir=$PRE_DIR            \
+#    pro_dir=$PRO_DIR            \
+#    fdname3d=$FDNAME3D_HY       \
+#    frstyear=$FRSTYEAR          \
+#    lstyear=$LSTYEAR            \
+#    case_name=$CASENAME         \
+#    ./ncl/take_3D_hybrid_from_raw_data-150921.ncl
+
 
