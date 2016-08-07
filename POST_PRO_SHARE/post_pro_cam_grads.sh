633b4a66ab2dd20ca050fa38e94f40a82cd3199b
Novarizark <dennis-1991@qq.com>
2016-08-07 23:47:56 +0800

    160807

--- POST_PRO_SHARE/post_pro_cam_grads.sh
+++ POST_PRO_SHARE/post_pro_cam_grads.sh
@@ -10,22 +10,23 @@
 
 # Path of the original data
 # Caution: DO NOT DELETE /" IN STRING!
-PRE_DIR=\"/HOME/sysu_hjkx_ys/WORKSPACE/L_Zealot/cesm/B/B_ALBD_STR_MONSOON-2015/run/\"
+PRE_DIR=\"/HOME/sysu_hjkx_ys/WORKSPACE/L_Zealot/cesm/F/F_AMIP/run/\"
 #PRE_DIR=\"/HOME/sysu_hjkx_ys/WORKSPACE/data/model/L_Zealot/SCS_ANNCYC-2015/pre/B2000_f09_CAM5PM_SCS_ANNCYC/\"
 
 # Path of the post processed data
-PRO_DIR=\"/HOME/sysu_hjkx_ys/WORKSPACE/L_Zealot/cesm/B/B_ALBD_STR_MONSOON-2015/post_data/\"
+PRO_DIR=\"/HOME/sysu_hjkx_ys/WORKSPACE/L_Zealot/temp/F_AMIP/\"
 
 # Case name
-CASENAME=\"B_ALBD_STR_MONSOON-2015\"
+CASENAME=\"F_AMIP\"
 
 
 # Names of 2D fields
-FDNAME2D="(/\"PRECL\",\"PRECC\",\"LHFLX\",\"PS\",\"PSL\",\"QFLX\",\"TS\",\"TMQ\"/)" #often use
+FDNAME2D="(/\"PRECT\",\"FLUT\"/)" #often use
 #FDNAME2D="(/\"TS\"/)" #often use
 
 # Names of 3D fields
-FDNAME3D="(/\"U\",\"V\",\"T\",\"OMEGA\",\"Q\",\"RELHUM\",\"Z3\"/)" #often use
+FDNAME3D="(/\"U\",\"V\",\"T\",\"Z3\"/)" #often use
+#FDNAME3D="(/\"U\",\"V\",\"T\",\"OMEGA\",\"Q\",\"RELHUM\",\"Z3\"/)" #often use
 #FDNAME3D="(/\"U\",\"T\",\"Z3\"/)" #often use
 
 # Names of 3D HY fields
@@ -41,10 +42,10 @@ PLEV="(/925,850,700,600,500,400,300,200,100,50/)"
 #PLEV="(/500,300,200/)"
 
 # First year of the subset
-FRSTYEAR=250
+FRSTYEAR=1979
 
 # Last year of the subset
-LSTYEAR=319
+LSTYEAR=2005
 
 # Process fig flag
 FLAG2D=1
