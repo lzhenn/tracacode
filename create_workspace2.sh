8fb6059a65393986be04e67bcfdc54d2d6ee24ca
Zhenning Li <zhenningli@merovingian.geog.berkeley.edu>
2016-10-24 11:21:06 -0700

    test2

---
+++ create_workspace2.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#-----------------------------------------------
+#   This is a shell script for setting up a
+# new workspace of one specific project, You 
+# should set the basic parameters as below. 
+# Good Luck!
+#               Last Modified on  2015-02-05
+#               A L_Zealot Product
+#-----------------------------------------------
+
+# Name of your workspace
+WORKNAME=WNP_COOL_jxw-2015
+
+# Now to create the workspace
+mkdir $WORKNAME
+cd $WORKNAME
+mkdir data ncl fig
+cd data
+mkdir model obv
+cd model
+mkdir pre pro
