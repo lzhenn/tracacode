622414e92c2fe4f92e556794a0b96359d18e3ef9
Novarizark <dennis-1991@qq.com>
2016-11-19 02:22:05 +0800

    161119

--- UTILITY-2016/nctime.sh
+++ UTILITY-2016/nctime.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 NC=\"`pwd`/$1\" #for the nc filename
-SCRIPT_DIR=$HOME/project/UTILITY-2016/ncl
+SCRIPT_DIR=$HOME_PROJECT/UTILITY-2016/ncl
 
 # -n no line number
 # -Q no copyright info
