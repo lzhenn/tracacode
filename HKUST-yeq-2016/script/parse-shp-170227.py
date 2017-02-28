98d0da3da8254d8463e94631e9d740e4ce2ca292
Novarizark <dennis-1991@qq.com>
2017-02-28 14:57:44 +0800

    170228

--- HKUST-yeq-2016/script/parse-shp-170227.py
+++ HKUST-yeq-2016/script/parse-shp-170227.py
@@ -1,4 +1,18 @@
-import shapely
-import fiona
+# First example using shapefile and shapely:
+import shapefile
+from shapely.geometry import Polygon, Point, MultiPolygon
 
+polygon = shapefile.Reader('../../UTILITY-2016/shp/PRD/PRD.shp') 
+polygon = polygon.shapes()  
+point = Point(119.0,23.0)
+shpfilePoints = []
+for shape in polygon:
+    shpfilePoints = shape.points 
+    polygon = shpfilePoints 
+    poly = Polygon(polygon)
 
+    # point in polygon test
+    if poly.contains(point):
+        print 'inside'
+    else:
+        print 'OUT'
