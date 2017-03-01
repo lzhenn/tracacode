324a038f63723ad795a900f82159fa0e5bb500f2
Novarizark <dennis-1991@qq.com>
2017-03-02 03:02:26 +0800

    170302

---
+++ HKUST-yeq-2016/script/parse-if-pt-in-shp-170227.py
@@ -0,0 +1,18 @@
+# First example using shapefile and shapely:
+import shapefile
+from shapely.geometry import Polygon, Point, MultiPolygon
+
+polygon = shapefile.Reader('../../UTILITY-2016/shp/PRD/PRD.shp') 
+polygon = polygon.shapes()  
+point = Point(114.0,23.0)
+shpfilePoints = []
+for shape in polygon:
+    shpfilePoints = shape.points 
+    polygon = shpfilePoints 
+    poly = Polygon(polygon)
+
+    # point in polygon test
+    if poly.contains(point):
+        print 'inside'
+    else:
+        print 'OUT'
