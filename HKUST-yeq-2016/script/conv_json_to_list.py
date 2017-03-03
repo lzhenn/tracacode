e1ed4b172c74ab260964e00002b872c44129c47e
Novarizark <dennis-1991@qq.com>
2017-03-04 03:02:28 +0800

    170304

--- HKUST-yeq-2016/script/conv_json_to_list.py
+++ HKUST-yeq-2016/script/conv_json_to_list.py
@@ -8,12 +8,21 @@
 #
 import json
 
+inv_path='./'
+
 #Parser
 with open(inv_path+'record.json', 'r') as f:
     pt_dic = json.load(f)
 
+pt_list=[]
+n=0
+fr=open('record.txt','w')
 for item in pt_dic.values():
-
-
-
-
+    lat=item['lat']
+    lon=item['lon']
+    value=item['value']
+    fr.write('%8.3f %8.3f %4d\n' % (lat, lon, value))
+    pt_list.append([item['lat'], item['lon'], item['value']])
+    n=n+1
+    
+fr.close()
