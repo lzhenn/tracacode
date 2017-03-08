7ce75bce4f69930ab4fdf1ee55f975fb13e2e236
Novarizark <dennis-1991@qq.com>
2017-03-08 15:07:20 +0800

    170308

--- HKUST-yeq-2016/script/run-hysplit4-wrf-multi-init.py
+++ HKUST-yeq-2016/script/run-hysplit4-wrf-multi-init.py
@@ -1,12 +1,17 @@
 import os
 import datetime
 
-start_time='2015-01-26 12:00:00'
+start_time='2015-01-01 00:00:00'
 end_time='2015-01-27 00:00:00'
+
+# Point Height
+height=100
 
 # Integration step in Hour
 int_step=12
 
+# CONTROL file sample directory (Don't use 'CONTROL')
+smp_path='CONTROL.smp'
 
 #Operation
 int_time_obj = datetime.datetime.strptime(start_time, '%Y-%m-%d %H:%M:%S')
@@ -14,15 +19,29 @@ end_time_obj = datetime.datetime.strptime(end_time, '%Y-%m-%d %H:%M:%S')
 time_delta=datetime.timedelta(hours=int_step)
 
 while int_time_obj <= end_time_obj:
+    print('-------------------------\n\n')
     print(int_time_obj)
-    fr=open('CONTROL','r+')
+    fr=open(smp_path,'r')
     lines=fr.readlines()
-    
-    lines[0]=int_time_obj.strftime('%y %m %d %H\n')
-    lines[-1]='trajout_'+int_time_obj.strftime('%y%m%d%H\n')
-    pos = fr.seek(0, 0);  # back to the file top
-    fr.writelines(lines)
     fr.close()
     
+    # Initial Date Change
+    lines[0]=int_time_obj.strftime('%y %m %d %H\n')
+    # Output Date Change
+    lines[-1]='trajout_'+int_time_obj.strftime('%y%m%d%H\n')
+    
+    # Height Change
+    npoints=int(lines[1])
+    print(npoints)
+    for idx, item in enumerate(lines):
+        if (idx >=2) and (idx<=(2+npoints-1)):
+            item_ind=item.split(' ')
+            item_ind[2]=height
+            lines[idx]=str(item_ind[0])+' '+str(item_ind[1])+' '+str(item_ind[2])+'\n'
+            
+    fr=open('CONTROL','w')
+    fr.writelines(lines)
+    fr.close()
     int_time_obj = int_time_obj+time_delta
+    
     os.system('C:/hysplit4/exec/hyts_std.exe')
