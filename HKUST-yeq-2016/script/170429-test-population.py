cf88a385e5eb12c3c008685e22eebe3c15aac2cc
Novarizark <dennis-1991@qq.com>
2017-05-02 03:01:35 +0800

    170502

--- HKUST-yeq-2016/script/170429-test-population.py
+++ HKUST-yeq-2016/script/170429-test-population.py
@@ -3,12 +3,59 @@
 #
 #
 #       L_Zealot
-#       Feb 28, 2017
+#       May 1, 2017
 #
 #
+from mpl_toolkits.basemap import Basemap, cm
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
 import scipy.io as sio 
+from netCDF4 import Dataset
+import numpy as np
 
-mat_contents=sio.loadmat('../data/obv/population_output_D1.mat')
 
+def read_latlon(pfname):
+    corfile= Dataset(pfname)
+    lat=corfile.variables['LAT'][0,0,:,:]
+    lon=corfile.variables['LON'][0,0,:,:]
+    return lat, lon
+
+pfname='/home/yangsong3/L_Zealot/data-mirror/data-model/L_Zealot/HKUST_yeq-2016/gridsys/GRIDCRO2D_3km'
+lat, lon = read_latlon(pfname)
+mat_contents=sio.loadmat('../data/obv/population_output_D4.mat')
 pop=mat_contents['population_output'] #list
-print(len(pop[0]))
+lat_0=28.5
+lon_0=114.0
+
+# create figure and axes instances
+fig = plt.figure(figsize=(8,8))
+ax = fig.add_axes([0.1,0.1,0.8,0.8])
+# create polar stereographic Basemap instance.
+m = Basemap(projection='stere',lon_0=lon_0,lat_0=lat_0,lat_ts=lat_0,\
+            llcrnrlat=lat.min(),urcrnrlat=lat.min(),\
+            llcrnrlon=lon.min(),urcrnrlon=lon.max(),\
+            rsphere=6371200.,resolution='l',area_thresh=10000)
+# draw coastlines, state and country boundaries, edge of map.
+m.drawcoastlines()
+m.drawstates()
+m.drawcountries()
+plt.savefig('../fig/population.png',bbox_inches='tight')
+# draw parallels.
+parallels = np.arange(0.,90,10.)
+m.drawparallels(parallels,labels=[1,0,0,0],fontsize=10)
+# draw meridians
+meridians = np.arange(180.,360.,10.)
+m.drawmeridians(meridians,labels=[0,0,0,1],fontsize=10)
+exit()
+ny = data.shape[0]; nx = data.shape[1]
+lons, lats = m.makegrid(nx, ny) # get lat/lons of ny by nx evenly space grid.
+x, y = m(lons, lats) # compute map proj coordinates.
+# draw filled contours.
+clevs = [0,1,2.5,5,7.5,10,15,20,30,40,50,70,100,150,200,250,300,400,500,600,750]
+cs = m.contourf(x,y,data,clevs,cmap=cm.s3pcpn)
+# add colorbar.
+cbar = m.colorbar(cs,location='bottom',pad="5%")
+cbar.set_label('mm')
+# add title
+plt.title(prcpvar.long_name+' for period ending '+prcpvar.dateofdata)
--- POST_PRO_SHARE/170418-post_pro_cam_esm_daily.sh
+++ POST_PRO_SHARE/170418-post_pro_cam_esm_daily.sh
@@ -20,13 +20,13 @@ PRO_DIR=\"/users/yangsong3/L_Zealot/F/DATA_Nudging/pro/\"
 CASENAME=\"Nudging\"
 
 # ESM folder prefix
-ESM_NAME=ESMn3_
+ESM_NAME=ESMn4_
 
 # ESM folder memebers
 ESM_STRT=1
 
 # ESM folder memebers
-ESM_LAST=15
+ESM_LAST=8
 
 
 
