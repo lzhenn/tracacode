05a9d70ba1a419012fb878002d9dd2d9244041a1
Novarizark <dennis-1991@qq.com>
2017-05-24 03:02:44 +0800

    170524

--- HKUST-yeq-2016/script/170522-population-integrate-quick-algrithm.py
+++ HKUST-yeq-2016/script/170522-population-integrate-quick-algrithm.py
@@ -100,7 +100,7 @@ while int_time_obj <= end_time_obj:
                         cor_y_pos0=cor_y_pos
             except:
                 continue;
-        if pos_line % 1000==0:
+        if pos_line % 10000==0:
             elapsed=time.clock()-strt_time
             print('Line %10d/%10d Time elapsed:%7.3fs Min_Dis=%5.3f' % (pos_line, len_line0, elapsed, min_dis))
 
