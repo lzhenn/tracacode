aa0f979ef00519a5ba37bb1aff94cb657adcf437
Novarizark <dennis-1991@qq.com>
2017-07-13 03:01:42 +0800

    170713

--- WNP_COOL_jxw-2015/script/scatter.py
+++ WNP_COOL_jxw-2015/script/scatter.py
@@ -1,14 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 fig = plt.figure()
 x = np.linspace(-np.pi, np.pi, 100)
 y = 2*np.sin(x)
 
-ax = fig.add_subplot(2, 2, 1)
+ax = fig.add_subplot(1, 1, 1)
 ax.set_title('centered spines')
-ax.plot(x, y)
+ax.plot(x, y, '.')
 ax.spines['left'].set_position('center')
 ax.spines['right'].set_color('none')
 ax.spines['bottom'].set_position('center')
@@ -18,41 +17,6 @@ ax.spines['bottom'].set_smart_bounds(True)
 ax.xaxis.set_ticks_position('bottom')
 ax.yaxis.set_ticks_position('left')
 
-ax = fig.add_subplot(2, 2, 2)
-ax.set_title('zeroed spines')
-ax.plot(x, y)
-ax.spines['left'].set_position('zero')
-ax.spines['right'].set_color('none')
-ax.spines['bottom'].set_position('zero')
-ax.spines['top'].set_color('none')
-ax.spines['left'].set_smart_bounds(True)
-ax.spines['bottom'].set_smart_bounds(True)
-ax.xaxis.set_ticks_position('bottom')
-ax.yaxis.set_ticks_position('left')
-
-ax = fig.add_subplot(2, 2, 3)
-ax.set_title('spines at axes (0.6, 0.1)')
-ax.plot(x, y)
-ax.spines['left'].set_position(('axes', 0.6))
-ax.spines['right'].set_color('none')
-ax.spines['bottom'].set_position(('axes', 0.1))
-ax.spines['top'].set_color('none')
-ax.spines['left'].set_smart_bounds(True)
-ax.spines['bottom'].set_smart_bounds(True)
-ax.xaxis.set_ticks_position('bottom')
-ax.yaxis.set_ticks_position('left')
-
-ax = fig.add_subplot(2, 2, 4)
-ax.set_title('spines at data (1, 2)')
-ax.plot(x, y)
-ax.spines['left'].set_position(('data', 1))
-ax.spines['right'].set_color('none')
-ax.spines['bottom'].set_position(('data', 2))
-ax.spines['top'].set_color('none')
-ax.spines['left'].set_smart_bounds(True)
-ax.spines['bottom'].set_smart_bounds(True)
-ax.xaxis.set_ticks_position('bottom')
-ax.yaxis.set_ticks_position('left')
 # ----------------------------------------------------
 
 
@@ -76,26 +40,5 @@ def adjust_spines(ax, spines):
     else:
         # no xaxis ticks
         ax.xaxis.set_ticks([])
-
-fig = plt.figure()
-
-x = np.linspace(0, 2*np.pi, 100)
-y = 2*np.sin(x)
-
-ax = fig.add_subplot(2, 2, 1)
-ax.plot(x, y, clip_on=False)
-adjust_spines(ax, ['left'])
-
-ax = fig.add_subplot(2, 2, 2)
-ax.plot(x, y, clip_on=False)
-adjust_spines(ax, [])
-
-ax = fig.add_subplot(2, 2, 3)
-ax.plot(x, y, clip_on=False)
-adjust_spines(ax, ['left', 'bottom'])
-
-ax = fig.add_subplot(2, 2, 4)
-ax.plot(x, y, clip_on=False)
-adjust_spines(ax, ['bottom'])
 
 plt.show()
