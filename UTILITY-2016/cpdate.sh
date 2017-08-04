2b397af7254f15cb7121bcf5b7e502128b8d2305
Novarizark <dennis-1991@qq.com>
2017-08-05 03:02:37 +0800

    170805

--- UTILITY-2016/cpdate.sh
+++ UTILITY-2016/cpdate.sh
@@ -2,4 +2,4 @@
 
 SOURCE=`pwd`/$1 #for the nc filename
 OBJ=`date +%y%m%d`-$2
-cp $SOURCE $OBJ
+cp -i $SOURCE $OBJ
--- UTILITY-2016/mvdate.sh
+++ UTILITY-2016/mvdate.sh
@@ -2,4 +2,4 @@
 
 SOURCE=`pwd`/$1 #for the nc filename
 OBJ=`date +%y%m%d`-$2
-mv $SOURCE $OBJ
+mv -i $SOURCE $OBJ
