8aa8dacfa945e7750cd544aa8aa50ab92f2787f6
Novarizark <dennis-1991@qq.com>
2017-11-07 03:01:42 +0800

    171107

--- ITMM-dt-2017/script/171024-select-j.py
+++ ITMM-dt-2017/script/171024-select-j.py
@@ -49,16 +49,21 @@ def main():
             pt=pd.read_csv(in_dir+get_file_name(sta_num, curr_year, corr_algthm, spe), parse_dates=True, skiprows=1, names=['time', 'max'], index_col='time')
             print('parsing '+in_dir+get_file_name(sta_num, curr_year, corr_algthm, spe))
             pt_out=reorg_rad(pt)
-            with open(out_dir+get_outfile_name(sta_num, curr_year, corr_algthm, spe), 'w') as f:
-                pt_out.to_csv(f)
+            fout_name=out_dir+get_outfile_name(sta_num, corr_algthm, spe)
+            if os.path.isfile(fout_name):
+                with open(fout_name, 'a') as f:
+                    pt_out.to_csv(f, header=False)
+            else:
+                with open(fout_name, 'w') as f:
+                    pt_out.to_csv(f)
         curr_year=str(int(curr_year)+1)
 
 def get_file_name(sta_num, curr_year, corr, spe):
     fname='j'+spe+'_'+curr_year+'_'+sta_num+'_'+corr+'_Hour.csv'
     return fname
 
-def get_outfile_name(sta_num, curr_year, corr, spe):
-    fname='j'+spe+'_'+curr_year+'_'+sta_num+'_'+corr+'_Daily.csv'
+def get_outfile_name(sta_num, corr, spe):
+    fname='j'+spe+'_'+sta_num+'_'+corr+'_Daily.csv'
     return fname
 
 def reorg_rad(pt):
