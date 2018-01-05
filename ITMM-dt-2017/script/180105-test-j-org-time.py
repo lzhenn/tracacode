9e543aff248534cc76a8cb9285e192e2fed71af8
Novarizark <dennis-1991@qq.com>
2018-01-06 03:01:18 +0800

    180106

--- ITMM-dt-2017/script/180105-test-j-org-time.py
+++ ITMM-dt-2017/script/180105-test-j-org-time.py
@@ -25,7 +25,7 @@ def main():
     sta_num='67606'
 
     # Start Year 
-    start_year='2011'
+    start_year='2015'
     
     # End Year
     end_year='2017'
@@ -34,12 +34,12 @@ def main():
     in_dir='../data/ITMM-dt-2017/'+sta_num+'/J/'
     
     # Output Dir 
-    out_dir='../data/ITMM-dt-2017/'+sta_num+'/J/'
+    out_dir='../data/ITMM-dt-2017/'+sta_num+'/J/pro_data/'
 
     # Correct Algrithm
     #   C1 -- Both j and splined
     #   C2 == Only j
-    corr_algthm='C2' 
+    corr_algthm='C1' 
 
     # Species
     species=['H2O2','HCHO_M','HCHO_R','HONO','NO3_M','NO3_R','NO2','O1D']
@@ -50,50 +50,36 @@ def main():
 #----------------------------------------------------
 # Main function
 #----------------------------------------------------
- 
+    curr_year='2011' 
 
-    # Preparation
-    curr_year=start_year
-    while curr_year<=end_year:
-        # Start Date in Julian Day 
-        start_time=curr_year+'001'
-        # End Date in Julian Day
-        end_time=curr_year+'366'
-        
-        int_time_obj = datetime.datetime.strptime(start_time, '%Y%j')
-        end_time_obj = datetime.datetime.strptime(end_time, '%Y%j')
-        time0 = datetime.datetime.strptime(time_base,'%Y%m%d%H%M')
-        file_time_delta=datetime.timedelta(days=1)
-        record_time_delta=datetime.timedelta(hours=23)
-        curr_time_obj=int_time_obj
-       
-        # Main Loop...
-        # Loop the individual species
-        for pos, spe in enumerate(species):
-            while curr_time_obj <= end_time_obj:
-                fout_name=out_dir+get_outfile_name(sta_num, curr_time_obj, corr_algthm, spe)
-                time0_str=curr_time_obj.strftime('%y%j')   
-                fn=in_dir+'j'+spe+time0_str+'.ded'
-                try:
-                    fr = open(fn, 'r')
-                except:
-                    curr_time_obj=curr_time_obj+file_time_delta
-                    continue
-                print('parsing '+fn+'...')
-                lines=fr.readlines()
-                fr.close()
-                df = org_data(lines, time0, spe, corr_algthm, sta_num)
-                df_min=df.resample('1Min').mean()    # Resample into hourly data
-                with open(fout_name, 'w') as f:
-                    df_min.to_csv(f)
-                # Point to next file
-                curr_time_obj=curr_time_obj+file_time_delta
-            curr_time_obj=int_time_obj
-        curr_year=str(int(curr_year)+1)
+    # Start Date in Julian Day 
+    start_time=curr_year+'333'
+    int_time_obj = datetime.datetime.strptime(start_time, '%Y%j')
+    time0 = datetime.datetime.strptime(time_base,'%Y%m%d%H%M')
+    curr_time_obj=int_time_obj
+    
+    # Main Loop...
+    # Loop the individual species
+    for pos, spe in enumerate(species):
+         fout_name=out_dir+get_outfile_name(sta_num, curr_time_obj, corr_algthm, spe)
+         time0_str=curr_time_obj.strftime('%y%j')   
+         fn=in_dir+'j'+spe+time0_str+'.ded'
+         try:
+             fr = open(fn, 'r')
+         except:
+             curr_time_obj=curr_time_obj+file_time_delta
+             continue
+         print('parsing '+fn+'...')
+         lines=fr.readlines()
+         fr.close()
+         df = org_data(lines, time0, spe, corr_algthm, sta_num)
+        # df_min=df.resample('1Min').mean()    # Resample into hourly data
+         with open(fout_name, 'w') as f:
+             df.to_csv(f)
 
 def get_outfile_name(sta_num, timestmp, corr, spe):
     time0=timestmp.strftime('%Y%m%d')   
-    fname='j'+spe+'_'+time0+'_'+sta_num+'_'+corr+'_Min.csv'
+    fname='j'+spe+'_'+time0+'_'+sta_num+'_'+corr+'_re-org.csv'
     return fname
 
 def get_file_name(timestmp, species):
@@ -117,10 +103,10 @@ def org_data(lines, time0, spe, corr_algthm, sta_num):
         timestamp.append(time_now)
         values.append(value)
     values=np.array(values)
-    if not(time_now >= datetime.datetime(2015,9,1)):
-        values=data_corr_algthm(values, corr_algthm, sta_num, spe)
-    else:
-        print(time_now)
+#    if not(time_now >= datetime.datetime(2015,9,1)):
+#        values=data_corr_algthm(values, corr_algthm, sta_num, spe)
+#    else:
+    print(time_now)
     df= pd.DataFrame(values,index=timestamp,columns=[spe])
     return df
 
