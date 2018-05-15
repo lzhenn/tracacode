b9960a3a4b8595caccf92a5188e0ccb6d9489afc
Novarizark <dennis-1991@qq.com>
2018-05-16 03:01:24 +0800

    180516

--- POST_PRO_SHARE/170525-post_pro_cam_pkg_continue_run_daily.sh
+++ POST_PRO_SHARE/170525-post_pro_cam_pkg_continue_run_daily.sh
@@ -11,20 +11,20 @@
 # Path of the original data
 # Caution: DO NOT DELETE \" IN STRING!
 #PRE_DIR=\"/HOME/sysu_hjkx_ys/WORKSPACE/L_Zealot/cesm/B/B2000_f09_CAM5_spin-up/run/\"
-PRE_DIR=\"/users/yangsong3/L_Zealot/F/AMIP_C5PM/exe/\"
+PRE_DIR=\"/users/yangsong3/L_Zealot/F/aquac4/exe/\"
 
 # Path of the post processed data
-PRO_DIR=\"/users/yangsong3/L_Zealot/F/AMIP_C5PM/post_data/\"
+PRO_DIR=\"/users/yangsong3/L_Zealot/F/aquac4/pro/\"
 
 # Case name
-CASENAME=\"AMIP_C5PM\"
+CASENAME=\"aquac4\"
 
 
 # Names of 2D fields
 #FDNAME2D="(/\"PRECL\",\"PRECC\",\"LHFLX\",\"PS\",\"PSL\",\"QFLX\",\"TS\",\"TMQ\"/)" #often use
 #FDNAME2D="(/\"TS\",\"TSMX\"/)" #often use
 #FDNAME2D="(/\"PRECT\",\"U850\",\"V850\"/)" #often use
-FDNAME2D="(/\"PRECC\",\"PRECL\"/)" #often use
+FDNAME2D="(/\"PRECC\",\"PRECL\",\"PSL\",\"TS\"/)" #often use
 
 # Names of 3D fields
 FDNAME3D="(/\"U\",\"V\",\"T\",\"OMEGA\",\"Q\",\"Z3\"/)" #often use
@@ -34,14 +34,14 @@ FDNAME3D="(/\"U\",\"V\",\"T\",\"OMEGA\",\"Q\",\"Z3\"/)" #often use
 #FDNAME3D_HY="(/\"U\",\"V\",\"T\"/)" # hybrid coordinate
 
 # First year of the subset
-FRSTYEAR=1979
+FRSTYEAR=6
 
 # Last year of the subset
-LSTYEAR=2005
+LSTYEAR=20
 
 # Layers of 3D fields
 # CAM4 = 26; CAM5 = 30
-LAYERS=30
+LAYERS=26
 
 # Output specific pressure layers
 # CAUTION: Do not leave species between element!
@@ -50,7 +50,7 @@ PLEV="(/1000,925,850,700,500,200/)"
 
 
 # Process flag
-FLAG_2D=0
+FLAG_2D=1
 FLAG_3D=1
 FLAG_3D_HY=0
 
