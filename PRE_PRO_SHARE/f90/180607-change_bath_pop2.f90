fbd4b0da344da624e117123c0095f0c229b00608
Novarizark <dennis-1991@qq.com>
2018-06-08 03:01:25 +0800

    180608

---
+++ PRE_PRO_SHARE/f90/180607-change_bath_pop2.f90
@@ -0,0 +1,23 @@
+program change_bathymetry 
+    implicit none
+    integer, parameter :: fin = 100, fout= 101
+    integer, parameter :: nlen = 122880
+    integer :: ii
+    integer :: status = 0
+    integer :: bath(nlen)=0
+    character(len=256) :: filename="/users/yangsong3/CESM/input/ocn/pop/gx1v6/grid/topography_20090204.ieeei4"
+    character(len=256) :: outname="/users/yangsong3/CESM/input/ocn/pop/gx1v6/grid/topography_surf_aqua_20180607.ieeei4"
+    
+    open(unit=fin, file=filename, form="unformatted", access="direct", recl=nlen, convert="big_endian")
+    read(fin, rec=1) bath 
+    close(fin)
+    do ii = 1, nlen
+        if (bath(ii)<1) then
+            bath(ii)=1
+        end if
+    end do
+    open(unit=fout, file=outname, form="unformatted", access="direct", action="write", recl=nlen, convert="big_endian")
+    write(fout, rec=1) bath
+    close(fout)
+
+end program
