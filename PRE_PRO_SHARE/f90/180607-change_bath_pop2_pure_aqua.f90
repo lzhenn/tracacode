b093743c5470a2965d6dbc25157bf6672c42324d
Novarizark <dennis-1991@qq.com>
2018-06-15 03:01:26 +0800

    180615

--- PRE_PRO_SHARE/f90/180607-change_bath_pop2_pure_aqua.f90
+++ PRE_PRO_SHARE/f90/180607-change_bath_pop2_pure_aqua.f90
@@ -7,20 +7,22 @@ program change_bathymetry
     integer :: bath(nlen)=0
     integer :: bath0(320,384)=0
     character(len=256) :: filename="/users/yangsong3/CESM/input/ocn/pop/gx1v6/grid/topography_20090204.ieeei4"
-    character(len=256) :: outname="/users/yangsong3/CESM/input/ocn/pop/gx1v6/grid/topography_surf_aqua_polar_20180607.ieeei4"
+    character(len=256) :: outname="/users/yangsong3/CESM/input/ocn/pop/gx1v6/grid/topography_pure_aqua_polar_20180607.ieeei4"
     
     open(unit=fin, file=filename, form="unformatted", access="direct", recl=nlen, convert="big_endian")
     read(fin, rec=1) bath 
     close(fin)
-    
-    do ii = 1, nlen
-        if (bath(ii)<1) then
-            bath(ii)=1
-        end if
-    end do
+   
+    bath=25
     bath0=reshape(bath,(/320,384/))
     bath0(:,1:2)=0
+    bath0(:,3)=5
+    bath0(:,4)=10
+    bath0(:,5)=18
     bath0(:,383:384)=0
+    bath0(:,382)=5
+    bath0(:,381)=10
+    bath0(:,380)=18
     open(unit=fout, file=outname, form="unformatted", access="direct", action="write", recl=nlen, convert="big_endian")
     write(fout, rec=1) bath0
     close(fout)
--- PRE_PRO_SHARE/f90/180607-change_region_mask_pop2.f90
+++ PRE_PRO_SHARE/f90/180607-change_region_mask_pop2.f90
@@ -5,8 +5,9 @@ program change_bathymetry
     integer :: ii
     integer :: status = 0
     integer :: idx(nlen)=0
+    integer :: idx0(320,384)
     character(len=256) :: filename="/users/yangsong3/CESM/input/ocn/pop/gx1v6/grid/region_mask_20090205.ieeei4"
-    character(len=256) :: outname="/users/yangsong3/CESM/input/ocn/pop/gx1v6/grid/region_mask_aqua_20180612.ieeei4"
+    character(len=256) :: outname="/users/yangsong3/CESM/input/ocn/pop/gx1v6/grid/region_mask_aqua_polar_20180612.ieeei4"
     
     open(unit=fin, file=filename, form="unformatted", access="direct", recl=nlen, convert="big_endian")
     read(fin, rec=1) idx 
@@ -14,9 +15,11 @@ program change_bathymetry
     
     idx(1:61440)=1
     idx(61441:)=2
-    
+    idx0=reshape(idx,(/320,384/))
+    idx0(:,1:2)=0
+    idx0(:,383:384)=0
     open(unit=fout, file=outname, form="unformatted", access="direct", action="write", recl=nlen, convert="big_endian")
-    write(fout, rec=1) idx
+    write(fout, rec=1) idx0
     close(fout)
 
 end program
