b3a55bad9d5999d6c6cec5edd981a452b53582de
Novarizark <dennis-1991@qq.com>
2018-06-21 03:01:23 +0800

    180621

--- PRE_PRO_SHARE/f90/180607-change_bath_pop2_pure_aqua.f90
+++ PRE_PRO_SHARE/f90/180607-change_bath_pop2_pure_aqua.f90
@@ -13,14 +13,14 @@ program change_bathymetry
     read(fin, rec=1) bath 
     close(fin)
    
-    bath=49
+    bath=1
     bath0=reshape(bath,(/320,384/))
     bath0(:,1:2)=0
     bath0(:,383:384)=0
-    do ii=1,48
-        bath0(:,ii+2)=ii
-        bath0(:,383-ii)=ii
-    end do
+    !do ii=1,10
+    !    bath0(:,3*ii:3*ii+2)=ii
+    !    bath0(:,383-ii*3:383-(ii*3-2))=ii
+    !end do
     open(unit=fout, file=outname, form="unformatted", access="direct", action="write", recl=nlen, convert="big_endian")
     write(fout, rec=1) bath0
     close(fout)
