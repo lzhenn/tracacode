7c7fcd305b57cc993a3dcc328ebddd6910b1bf1f
Novarizark <dennis-1991@qq.com>
2018-11-09 03:03:27 +0800

    181109

--- PRE_PRO_SHARE/f90/180607-change_bath_pop2_pure_aqua_mixed_layer.f90
+++ PRE_PRO_SHARE/f90/180607-change_bath_pop2_pure_aqua_mixed_layer.f90
@@ -7,15 +7,15 @@ program change_bathymetry
     integer :: bath(nlen)=0
     integer :: bath0(320,384)=0
     character(len=256) :: filename="/users/yangsong3/CESM/input/ocn/pop/gx1v6/grid/topography_20090204.ieeei4"
-    character(len=256) :: outname="/users/yangsong3/CESM/input/ocn/pop/gx1v6/grid/topography_pure_aqua_polar_mixed_layer_20180607.ieeei4"
+    character(len=256) :: outname="/users/yangsong3/CESM/input/ocn/pop/gx1v6/grid/topography_deep_aqua_polar_20181108.ieeei4"
     
     open(unit=fin, file=filename, form="unformatted", access="direct", recl=nlen, convert="big_endian")
     read(fin, rec=1) bath 
     close(fin)
     
     do ii = 1, nlen
-        if (bath(ii)<27) then
-            bath(ii)=27
+        if (bath(ii)<49) then
+            bath(ii)=49
         end if
     end do
     bath0=reshape(bath,(/320,384/))
