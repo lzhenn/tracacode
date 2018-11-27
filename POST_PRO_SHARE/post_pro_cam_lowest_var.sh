63de0538a997e19828e2f2d952779b052d428fe8
Novarizark <dennis-1991@qq.com>
2018-11-28 03:01:23 +0800

    181128

--- POST_PRO_SHARE/post_pro_cam_lowest_var.sh
+++ POST_PRO_SHARE/post_pro_cam_lowest_var.sh
@@ -10,14 +10,14 @@
 
 # Path of the original data
 # Caution: DO NOT DELETE \" IN STRING!
-PRE_DIR=\"/HOME/sysu_hjkx_ys/WORKSPACE/data/model/L_Zealot/SCS_ANNCYC-2015/pre/B2000_f09_CAM5PM_spin-up/\"
+PRE_DIR=\"/home/lzhenn/array/lzhenn/data/CONV_MON_WRK-2018/REAL_WORLD_SCYC/\"
 #PRE_DIR=\"/HOME/sysu_hjkx_ys/WORKSPACE/data/model/L_Zealot/SCS_ANNCYC-2015/pre/B2000_f09_CAM5PM_SCS_ANNCYC/\"
 
 # Path of the post processed data
-PRO_DIR=\"/HOME/sysu_hjkx_ys/WORKSPACE/data/model/L_Zealot/SCS_ANNCYC-2015/pro/\"
+PRO_DIR=\"/home/lzhenn/array/lzhenn/data/CONV_MON_WRK-2018/REAL_WORLD_SCYC/pro/\"
 
 # Case name
-CASENAME=\"B2000_f09_CAM5PM_spin-up\"
+CASENAME=\"B20f19-realworld\"
 
 
 # Names of 3D fields need to be extracted
@@ -27,13 +27,13 @@ FDNAME2D="(/\"U\",\"V\"/)" #often use
 
 # Layers of 3D fields
 # CAM4 = 26; CAM5 = 30
-LAYERS=30
+LAYERS=26
 
 # First year of the subset
-FRSTYEAR=201
+FRSTYEAR=1
 
 # Last year of the subset
-LSTYEAR=250
+LSTYEAR=100
 
 #-----------------------------------------------------------
 
