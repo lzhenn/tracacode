e5c17bcf0cbd8cc2498b5959ac34b035f81f867e
novarizark <lzhenn@mail2.sysu.edu.cn>
2019-12-16 03:21:03 +0800

    191216

--- 1911-COAWST/fig/form_gif.sh
+++ 1911-COAWST/fig/form_gif.sh
@@ -1,8 +1,16 @@
-for((I=0;I<=120;I++))
+
+PREFIX=ctrl_d01_SST_
+STEP_H=1
+TOTAL_STEP=120
+FRAME_DT=$(expr 15 \* ${STEP_H})
+
+for((I=0;I<=${TOTAL_STEP};I++))
 do
-    TFSTMP=`printf "%.3d" $I`
+    TF=$(expr $I \* ${STEP_H})
+    TFSTMP=`printf "%.3d" $TF`
     echo $TFSTMP
-    convert -trim +repage -bordercolor white -background white -flatten sen_d01_SLP_${TFSTMP}H.png sen_d01_noborder_${TFSTMP}H.png
+    convert -trim +repage -bordercolor white -background white -flatten ${PREFIX}${TFSTMP}H.png ${PREFIX}noborder_${TFSTMP}H.png
 done
-convert -delay 15 sen_d01_noborder_* result.gif
+
+convert -delay ${FRAME_DT} ${PREFIX}noborder_* result.gif
 convert result.gif -fuzz 10% -layers Optimize result.gif
