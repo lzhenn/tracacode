28f799df490e578d5d4920d87041d3925a9af5c9
novarizark <lzhenn@mail2.sysu.edu.cn>
2020-01-18 03:21:04 +0800

    200118

--- 1911-COAWST/script/200116-sandy-obv.py
+++ 1911-COAWST/script/200116-sandy-obv.py
@@ -29,26 +29,37 @@ def main():
     
     obv_path='../data/1911-COAWST/sandy/sandy.obv.trck'
     sen_path='../data/1911-COAWST/sandy/sandy.cpl.trck'
+    ctrl_path='../data/1911-COAWST/sandy/sandy.wrf.trck'
 
     dti = pd.date_range('2012-10-28 12:00:00', periods=97, freq='30min')
 
 
     df_obv=pd.read_csv(obv_path,parse_dates=True,index_col='time')
     df_cpl=pd.read_csv(sen_path, sep='\s+')
+    df_wrf=pd.read_csv(ctrl_path, sep='\s+')
     df_cpl['time']=dti
     df_cpl.set_index('time', inplace=True, drop=True)
+    
+    df_wrf['time']=dti
+    df_wrf.set_index('time', inplace=True, drop=True)
+    
     fig, ax = plt.subplots()
     df_obv_period=df_obv[((df_obv.index>=dti[0])&(df_obv.index<=dti[-1]))]
     df_obv_period=df_obv_period.astype({'slp': 'float'})
     df_obv_period=df_obv_period.dropna()
     
     plt.plot(df_cpl['slp'], label='CPL', color='red')
+    plt.plot(df_wrf['slp'], label='WRF_ONLY', color='blue')
     plt.plot(df_obv_period['slp'], label='IBTrACS obv', marker='o', color='black')
+
     plt.legend(loc='best', fontsize=SMFONT)
     plt.xlabel('Time',fontsize=SMFONT)
     plt.ylabel('SLP (hPa)',fontsize=SMFONT)
-    plt.xticks(fontsize=SMFONT)
+    plt.xticks(fontsize=SMFONT,rotation=-30)
     plt.yticks(fontsize=SMFONT)
+    
+   # pletp(ax.get_xticklabels(), rotation=-60, ha="right",
+   # rotation_mode="anchor")
     
     plt.title('TC Strength Evolution', fontsize=BIGFONT)
 #    fig.tight_layout()
