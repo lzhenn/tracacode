9b60a43c149ff8668700e3679ed7b4e89a5318a7
novarizark <lzhenn@mail2.sysu.edu.cn>
2020-01-19 03:21:03 +0800

    200119

--- 1911-COAWST/fig/sandy/form_gif.sh
+++ 1911-COAWST/fig/sandy/form_gif.sh
@@ -1,15 +1,12 @@
 
-PREFIX=sen_d01_HWave_
-STEP_H=6
-TOTAL_STEP=20
-FRAME_DT=$(expr 15 \* ${STEP_H})
-
+PREFIX=sandy_d01_SLP_
+TOTAL_STEP=97
+FRAME_DT=15
 for((I=0;I<=${TOTAL_STEP};I++))
 do
-    TF=$(expr $I \* ${STEP_H})
-    TFSTMP=`printf "%.3d" $TF`
+    TFSTMP=`printf "%.3d" $I`
     echo $TFSTMP
-    convert -trim +repage -bordercolor white -background white -flatten ${PREFIX}${TFSTMP}H.png ${PREFIX}noborder_${TFSTMP}H.png
+    convert -trim +repage -bordercolor white -background white -flatten ${PREFIX}${TFSTMP}Frm.png ${PREFIX}noborder_${TFSTMP}Frm.png
 done
 
 convert -delay ${FRAME_DT} ${PREFIX}noborder_* result.gif
