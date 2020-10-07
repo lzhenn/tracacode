eb3aa23a945bfb2fa092e56bec40091322172d81
novarizark <lzhenn@mail2.sysu.edu.cn>
2020-10-08 03:21:14 +0800

    201008

--- 1911-yeq-HALOGEN/script/201006-resident-depletion-scatter.py
+++ 1911-yeq-HALOGEN/script/201006-resident-depletion-scatter.py
@@ -60,29 +60,32 @@ def main():
     mcip_grid_fn="/disk/hq247/yhuangci/resource/map_info/research_domains/27km/GRIDCRO2D.27km"
     coast_dis_fn     ="/disk/hq247/yhuangci/analy/halogen/result/ncl_files/v1/calc/dis_to_coast_27km.nc"
     out_res_path="/disk/hq247/yhuangci/lzhenn/workspace/easy-wrf-trck/outnc/"
-    cmaq_with_file="../../../data/cmaq/with/jan/COMBINE_CCTM_ACONC_27km_2016*"  
-    cmaq_without_file="../../../data/cmaq/without/jan/COMBINE_CCTM_ACONC_27km_2016*"  
+    cmaq_with_file="/disk/hq247/yhuangci/analy/halogen/result/data/cmaq/with/"  
+    cmaq_without_file="/disk/hq247/yhuangci/analy/halogen/result/data/cmaq/without/"  
     
     line_types=['r-^','m:s','b-.*','g--o']
     months=['Jan', 'Apr', 'Jul', 'Oct']
+    smonths=['jan', 'apr', 'jul', 'oct']
     work_yyyymm=['01', '04', '07', '10']
-    
-    ds_dis=xr.open_dataset(coast_dis_fn)
-    var_dis=ds_dis['dis_to_coast']
-
-    ds_mcip_grid=xr.open_dataset(mcip_grid_fn)
-    mcip_lat=ds_mcip_grid['LAT'][0,0,:,:]
-    mcip_lon=ds_mcip_grid['LON'][0,0,:,:]
     
     fig, ax=plt.subplots()
     fig.subplots_adjust(left=0.08, bottom=0.18, right=0.97, top=0.92, wspace=None, hspace=None) 
         
-    for line_type, mon, itm in zip(line_types, months, work_yyyymm):
+    for line_type, mon, smon, itm in zip(line_types, months, smonths, work_yyyymm):
         
-        ds_with=xr.open_mfdataset(cmaq_with_file)
-        ds_without=xr.open_mfdataset(out_res_path+'*2016'+itm+'*')
+        ds_with=xr.open_mfdataset(cmaq_with_file+smon+'/COMBINE_CCTM_ACONC_27km_2016*',concat_dim='TSTEP')
+        ds_without=xr.open_mfdataset(cmaq_without_file+smon+'/COMBINE_CCTM_ACONC_27km_2016*',concat_dim='TSTEP')
+        var_with=ds_with['O3']
+        var_without=ds_without['O3']
+
+        var_with=var_with.mean(dim='TSTEP')
+        var_without=var_without.mean(dim='TSTEP')
+        var_diff=(var_with-var_without)*1000 # convert to ppb
+        var_surf_diff=var_diff[0,:,:]
+        var_pbl_diff=var_diff[0:19,:,:].mean(dim='LAY')
+        print(var_pbl_diff)
         
-        
+
         ds_res=xr.open_mfdataset(out_res_path+'*2016'+itm+'*')
         var_res=ds_res['OcnResTime']
         var_res=var_res.sum(dim='Time')
@@ -90,36 +93,35 @@ def main():
 
         var_res=var_res.loc[8:228,18:308]
         res_arr1d=var_res.values.flatten()
-        dis_arr1d=27*var_dis.values.flatten()
         
         # get bins
-        bin_means, bin_edges, binnumber = stats.binned_statistic(dis_arr1d,
-                                res_arr1d, statistic='mean', bins=300)
-       
+        bin_means, bin_edges, binnumber = stats.binned_statistic(res_arr1d, 
+                var_pbl_diff.values.flatten(), statistic='mean', bins=30)
+        
+        bin_std, bin_edges, binnumber = stats.binned_statistic(res_arr1d,
+                var_pbl_diff.values.flatten(), statistic='std', bins=30)
+
+
         #bin_max, bin_edges, binnumber = stats.binned_statistic(dis_arr1d,
         #                        res_arr1d, statistic='max', bins=300)
 
         #bin_min, bin_edges, binnumber = stats.binned_statistic(dis_arr1d,
         #                        res_arr1d, statistic='min', bins=300)
         
-        bin_std, bin_edges, binnumber = stats.binned_statistic(dis_arr1d,
-                                res_arr1d, statistic='std', bins=300)
-
         x_mid=(bin_edges[0:-1]+bin_edges[1:])/2
        
         ax.plot(x_mid,bin_means, line_type, label=mon, linewidth=3, markersize=10)
         ax.fill_between(x_mid, bin_means+bin_std, bin_means-bin_std, color=line_type[0:1], alpha=0.2) 
-        break
 
     plt.legend(loc='best', fontsize=SMFONT)
-    plt.xlabel('Distance to Coastline (km)',fontsize=SMFONT)
-    plt.ylabel('Ocean Sourced Air Occupation Ratio (%)',fontsize=SMFONT)
+    plt.xlabel('Ocean Sourced Air Occupation Ratio (%)',fontsize=SMFONT)
+    plt.ylabel('Ozone Depletion (ppb)',fontsize=SMFONT)
     plt.xticks(fontsize=SMFONT)
     plt.yticks(fontsize=SMFONT)
-    plt.xlim((-1000, 1000)) 
-    plt.title('Ocean Sourced Air Occupation Ratio - Distance to Coastline', fontsize=BIGFONT)
+    plt.xlim((0, 60)) 
+    plt.title('PBL Ozone Depletion - Ocean Sourced Air Occupation Ratio', fontsize=BIGFONT)
     fig.set_size_inches(FIG_WIDTH, FIG_HEIGHT)
-    fig.savefig('../fig/range_dis_ocean_occupy.png')
+    fig.savefig('../fig/ocean_occupy_pbl_o3_depletion.png')
     #plt.show()
 
 if __name__ == "__main__":
