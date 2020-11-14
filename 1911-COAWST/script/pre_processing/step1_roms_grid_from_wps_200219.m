283af0a460763b0a809b57ff6b7d519509c2b96e
novarizark <lzhenn@mail2.sysu.edu.cn>
2020-11-15 03:21:06 +0800

    201115

---
+++ 1911-COAWST/script/pre_processing/step1_roms_grid_from_wps_200219.m
@@ -0,0 +1,72 @@
+addpath(genpath('/users/b145872/project/1911-COAWST/script/mfiles'));
+netcdf_load('/users/b145872/project-dir/app/WPS/geo_em.d03.nc')
+figure
+pcolorjw(XLONG_M,XLAT_M,double(1-LANDMASK))
+hold on
+title('WRF LANDMASK grid, mercator proj')
+xlabel('longitude'); ylabel('latitiude')
+
+% pick connors, lat lon
+xl= 125.0; xr=140.0;
+yb= 12.0; yt= 20.0;
+
+% pick numbers, roms nrow x ncol
+numx=555; numy=296;
+% Call generic grid creation.
+roms_grid='/users/b145872/project-dir/app/COAWST-test/Projects/GONI_2020/goni_roms_domain.nc';
+
+
+
+% make matrix
+dx=(xr-xl)/numx; dy=(yt-yb)/numy;
+[lon, lat]=meshgrid(xl:dx:xr, yb:dy:yt);
+lon=lon.';lat=lat.';
+plot(lon,lat,'k-')
+plot(lon',lat','k-')
+text(120,15,'- - - roms grid')
+rho.lat=lat; rho.lon=lon;
+rho.depth=zeros(size(rho.lon)); % for now just make zeros
+rho.mask=zeros(size(rho.lon)); % for now just make zeros
+spherical='T';
+%projection='lambert conformal conic';
+projection='mercator';
+save temp_jcw33.mat rho spherical projection
+eval(['mat2roms_mw(''temp_jcw33.mat'',''',roms_grid,''');'])
+!del temp_jcw33.mat
+%User needs to edit roms variables
+disp([' '])
+disp(['Created roms grid --> ',roms_grid])
+disp([' '])
+disp(['You need to edit depth in ',roms_grid])
+disp([' '])
+
+% ROMS grid masking
+% Start out with the WRF mask.
+F = scatteredInterpolant(double(XLONG_M(:)),double(XLAT_M(:)), double(LANDMASK(:)),'nearest');
+roms_mask=F(lon,lat);
+
+%roms_mask=bwareaopen(roms_mask,5); %erode small islands
+
+L=bwlabel(roms_mask);
+stats=regionprops(L,'Area');                    %Get every label's Area
+allArea=[stats.Area];               
+allArea=sort(allArea,'descend')                 %Print island Area
+
+roms_mask=1-roms_mask;   % reverse land-sea mask value in roms
+
+figure
+pcolorjw(lon,lat,roms_mask)
+title('ROMS 1-LANDMASK grid, Mercator proj')
+xlabel('longitude'); ylabel('latitiude')
+
+% Compute mask on rho, u, v, and psi points
+
+water = double(roms_mask);
+u_mask = water(1:end-1,:) & water(2:end,:);
+v_mask= water(:,1:end-1) & water(:,2:end);
+psi_mask= water(1:end-1,1:end-1) & water(1:end-1,2:end) & water(2:end,1:end-1) & water(2:end,2:end);
+ncwrite(roms_grid,'mask_rho',roms_mask);
+ncwrite(roms_grid,'mask_u',double(u_mask));
+ncwrite(roms_grid,'mask_v',double(v_mask));
+ncwrite(roms_grid,'mask_psi',double(psi_mask));
+
---
+++ 1911-COAWST/script/pre_processing/step4_LP_smooth_bath_200219.m
@@ -0,0 +1,22 @@
+raw_fn='/users/b145872/project-dir/app/COAWST-GBA/Projects/GBA/roms-grid/GBA_roms_grid_test.nc';
+out_fn='/users/b145872/project-dir/app/COAWST-GBA/Projects/GBA/roms-grid/GBA_roms_grid_rx0_0d10.nc'
+
+[status,cmdout] = system(['cp ' raw_fn ' ' out_fn]);
+status
+
+netcdf_load(raw_fn);
+
+rx0max=0.1;
+disp(['Target for rx0=' num2str(rx0max)]);
+
+disp('Using LP method with heuristic');
+
+h0=GRID_LinProgHeuristic_rx0(mask_rho, h, rx0max);
+
+figure
+pcolorjw(lon_rho,lat_rho,h0-h)
+hold on
+caxis([-100 100]); colorbar
+title('ROMS bathy diff')
+xlabel('longitude'); ylabel('latitiude')
+ncwrite(out_fn,'h',h0);
