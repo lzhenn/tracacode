1ecb61360359ee05364dca2d4fa2527824288f68
novarizark <novarizark@gmail.com>
2020-12-13 03:31:04 +0800

    201213

--- 1911-COAWST/script/pre_processing/step4_LP_smooth_bath_200219.m
+++ 1911-COAWST/script/pre_processing/step4_LP_smooth_bath_200219.m
@@ -1,12 +1,13 @@
-raw_fn='/users/b145872/project-dir/app/COAWST-GBA/Projects/GBA/roms-grid/GBA_roms_grid_test.nc';
-out_fn='/users/b145872/project-dir/app/COAWST-GBA/Projects/GBA/roms-grid/GBA_roms_grid_rx0_0d10.nc'
+addpath(genpath('/home/metctm1/array/project/1911-COAWST/script/mfiles'));
+raw_fn='/home/metctm1/array/app/COAWST/COAWST201205/Projects/GBA/roms-grid-ust/roms_domain.nc';
+out_fn='/home/metctm1/array/app/COAWST/COAWST201205/Projects/GBA/roms-grid-ust/roms_domain_lp0.08.nc'
 
 [status,cmdout] = system(['cp ' raw_fn ' ' out_fn]);
 status
 
 netcdf_load(raw_fn);
 
-rx0max=0.1;
+rx0max=0.08;
 disp(['Target for rx0=' num2str(rx0max)]);
 
 disp('Using LP method with heuristic');
