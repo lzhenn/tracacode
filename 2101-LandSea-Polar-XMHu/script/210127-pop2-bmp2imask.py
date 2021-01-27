e11d1aab14fc40466b884f8659692c45b128f154
novarizark <novarizark@gmail.com>
2021-01-28 03:31:15 +0800

    210128

---
+++ 2101-LandSea-Polar-XMHu/script/210127-pop2-bmp2imask.py
@@ -0,0 +1,19 @@
+import numpy as np
+import xarray as xr
+from PIL import Image, ImageDraw 
+
+im_w=384
+im_h=320
+ds=xr.open_dataset('/home/metctm1/array/data/2101-LandSea-Polar-XMHu/gx1v6_larger_aus_090205.nc')
+imask=ds.grid_imask
+
+im = Image.open('../fig/bitmap_large_aus.bmp')
+values=list(im.getdata())
+
+for x in range(im_w):
+    for y in range(im_h):
+        imask.values[x*im_h+y]=values[y*im_w+x]
+
+imask.values=imask.values/255
+ds.to_netcdf('/home/metctm1/array/data/2101-LandSea-Polar-XMHu/gx1v6_larger_aus_090205.nc')
+
---
+++ 2101-LandSea-Polar-XMHu/script/210127-pop2-imask2bmap.py
@@ -0,0 +1,15 @@
+import numpy as np
+import xarray as xr
+from PIL import Image, ImageDraw 
+
+im_w=384
+im_h=320
+ds=xr.open_dataset('/home/metctm1/array/data/2101-LandSea-Polar-XMHu/gx1v6_larger_aus_090205_test.nc')
+imask=ds.grid_imask.values.reshape((im_w,im_h))
+
+image = Image.new('1', (im_w, im_h), 0)
+draw = ImageDraw.Draw(image)
+for x in range(im_w):
+    for y in range(im_h):
+        draw.point((x, y), fill=int(imask[x,y]))
+image.save('../fig/bitmap_test.bmp', 'bmp')
