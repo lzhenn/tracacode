9c1d9f0ba8c946272b1b708dfa1b6acb194c7e53
novarizark <novarizark@gmail.com>
2021-02-07 03:31:29 +0800

    210207

--- 2101-LandSea-Polar-XMHu/script/210205-draw-ts.py
+++ 2101-LandSea-Polar-XMHu/script/210205-draw-ts.py
@@ -4,21 +4,6 @@ import numpy as np
 import cartopy.crs as ccrs
 
 
-def sample_data(shape=(73, 145)):
-    """Return ``lons``, ``lats`` and ``data`` of some fake data."""
-    nlats, nlons = shape
-    lats = np.linspace(-np.pi / 2, np.pi / 2, nlats)
-    lons = np.linspace(0, 2 * np.pi, nlons)
-    lons, lats = np.meshgrid(lons, lats)
-    wave = 0.75 * (np.sin(2 * lats) ** 8) * np.cos(4 * lons)
-    mean = 0.5 * np.cos(2 * lats) * ((np.sin(2 * lats)) ** 2 + 2)
-
-    lats = np.rad2deg(lats)
-    lons = np.rad2deg(lons)
-    data = wave + mean
-
-    return lons, lats, data
-
 
 def main():
     fig = plt.figure(figsize=(10, 5))
