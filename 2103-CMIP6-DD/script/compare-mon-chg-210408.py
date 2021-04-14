f86ee8989a031e8fa99e7bd8484bb1c5b5285ec4
novarizark <novarizark@gmail.com>
2021-04-15 03:31:33 +0800

    210415

--- 2103-CMIP6-DD/script/compare-mon-chg-210408.py
+++ 2103-CMIP6-DD/script/compare-mon-chg-210408.py
@@ -13,11 +13,11 @@ SMFONT=14
 
 MAP_RES='110m'
 FIG_FMT='png'
-
+MON='Sep'
 CMIP_ARC='/home/metctm1/array/data/cmip6/cmip6-mpi-esm-hr/'
 
 ds_ref=xr.load_dataset(CMIP_ARC+'ts_Amon_MPI-ESM1-2-HR_ssp245_r1i1p1f1_gn_202001-202412.nc')
-ds_sen=xr.load_dataset(CMIP_ARC+'ts_Amon_MPI-ESM1-2-HR_ssp585_r1i1p1f1_gn_204501-204912.nc')
+ds_sen=xr.load_dataset(CMIP_ARC+'ts_Amon_MPI-ESM1-2-HR_ssp245_r1i1p1f1_gn_204501-204912.nc')
 
 ts_ref=ds_ref['ts'].loc['2020-09-16',:,:]-273.15
 ts_sen=ds_sen['ts'].loc['2045-09-16',:,:]-273.15
@@ -37,12 +37,12 @@ cf=ax.contourf(ts_ref.lon, ts_ref.lat, ts_diff,
     levels=levels, extend='both', transform=ccrs.PlateCarree(), cmap=cmap)
 ax.coastlines()
 ax.set_global()
-plt.title('MPI-ESM1-2-HR 2045 (SSP585) minus 2020 (SSP245) Sep TS',fontsize=MIDFONT)
+plt.title('MPI-ESM1-2-HR 2045 (SSP245) minus 2020 (SSP245) '+MON+' TS',fontsize=MIDFONT)
 
 # add color bar in final
 cax=fig.add_axes([0.15, 0.05, 0.7, 0.051])# left, down, right, up
 cbar = fig.colorbar(cf,ticks=range(-4, 5), cax=cax, orientation='horizontal')
 
-plt.savefig('../fig/ts.'+FIG_FMT, dpi=90, bbox_inches='tight')
+plt.savefig('../fig/ts.'+MON+'.'+FIG_FMT, dpi=90, bbox_inches='tight')
 plt.close('all')
 
