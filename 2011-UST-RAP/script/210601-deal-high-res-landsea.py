469f73f64b300fcb6ab603191b364412c4947885
novarizark <novarizark@gmail.com>
2021-06-02 03:31:32 +0800

    210602

--- 2011-UST-RAP/script/210601-deal-high-res-landsea.py
+++ 2011-UST-RAP/script/210601-deal-high-res-landsea.py
@@ -1,5 +1,9 @@
 
 import scipy.io as sp 
 ls_mat = '/home/metctm1/array/data/Calypso/land.mat'
-lsmask = sp.loadmat(ls_mat)
-print(lsmask)
+lslat_mat = '/home/metctm1/array/data/Calypso/lat.mat'
+lslon_mat = '/home/metctm1/array/data/Calypso/lon.mat'
+lsmask = sp.loadmat(ls_mat)['land']
+lat2d = sp.loadmat(lslat_mat)['lat']
+lon2d = sp.loadmat(lslon_mat)['lon']
+print(lat2d)
