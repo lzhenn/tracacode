cf4090f2926864db210e1bca8acc9a4eabea31e6
novarizark <novarizark@gmail.com>
2021-06-03 03:31:31 +0800

    210603

--- 2011-UST-RAP/script/210601-deal-high-res-landsea.py
+++ 2011-UST-RAP/script/210601-deal-high-res-landsea.py
@@ -1,9 +1,12 @@
 
 import scipy.io as sp 
+rlonw, rlone, rlats, rlatn=
 ls_mat = '/home/metctm1/array/data/Calypso/land.mat'
 lslat_mat = '/home/metctm1/array/data/Calypso/lat.mat'
 lslon_mat = '/home/metctm1/array/data/Calypso/lon.mat'
 lsmask = sp.loadmat(ls_mat)['land']
 lat2d = sp.loadmat(lslat_mat)['lat']
 lon2d = sp.loadmat(lslon_mat)['lon']
+
+
 print(lat2d)
