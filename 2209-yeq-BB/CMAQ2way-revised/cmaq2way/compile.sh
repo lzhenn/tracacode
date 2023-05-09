d98b9849cad8a9c056c8e9b5c39fe8c8995a57e6
Novarizark <novarizark@gmail.com>
2023-05-09 16:53:35 +0800

    refresh

--- 2209-yeq-BB/CMAQ2way-revised/cmaq2way/compile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-source /home/yhuangci/bashrc_intel_amd
-./clean -a
-export WRF_CMAQ=1
-export wrf_path=/home/lzhenn/array74/tracacode/2209-yeq-BB/CMAQ2way-revised/cmaq2way
-./wrfcmaq_twoway_coupler/assemble
-./configure
-
